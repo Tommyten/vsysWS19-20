92a62ec5c588241cdf4cc5061640dde4109e407d
Thomas <thomas.hormes@hotmail.de>
2020-02-03 11:57:37 +0100

    first commit

---
+++ src/main/java/example/App.java
@@ -0,0 +1,61 @@
+package example;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.datastax.spark.connector.japi.CassandraJavaUtil.javaFunctions;
+import static com.datastax.spark.connector.japi.CassandraJavaUtil.mapToRow;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String genomeSeqPattern = "[ACGT]{20,}";
+    private static final String genomeIdPatternString = "@((.+?-){3}.+?)\\s";
+    static Pattern genomeIdPattern = Pattern.compile(genomeIdPatternString);
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setMaster("local[2]").setAppName("ReadTest");
+        JavaSparkContext context = new JavaSparkContext(conf);
+
+        List<Genome> genomeList = new ArrayList<>();
+
+        JavaRDD<String> lines = context.textFile("testProject/target/classes/test.fastq");
+
+        String key = "";
+        String value = "";
+        boolean takeNext = false;
+
+        List<String> lineList = lines.collect();
+
+        for (String line :
+                lineList) {
+            Matcher matcher = genomeIdPattern.matcher(line);
+            if (matcher.matches()) {
+                key = matcher.group(0);
+                System.out.println("\n\n\nFound genomeID: " + key + "\n\n\n");
+            } else if (line.matches(genomeSeqPattern)) {
+                value = line;
+                System.out.println("\n\n\nFound genome: " + value + "\n\n\n");
+            }
+
+            if (!key.isEmpty() && !value.isEmpty()) {
+                genomeList.add(new Genome(key, value));
+                key = "";
+                value = "";
+                System.out.println("\n\n\nAdded genome!\n\n\n");
+            }
+        }
+
+        System.out.println("\n\n\n\nLIST LENGTH: " + genomeList.size() + "\n\n\n");
+        JavaRDD<Genome> rdd = context.parallelize(genomeList);
+        javaFunctions(rdd).writerBuilder("genomes", "kv", mapToRow(Genome.class)).saveToCassandra();
+    }
+}
