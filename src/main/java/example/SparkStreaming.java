796c8eededa70f0051745c8276f1a9c6dace4619
Thomas <thomas.hormes@hotmail.de>
2020-02-26 17:52:35 +0100

    added streaming job

--- src/main/java/example/SparkStreaming.java
+++ src/main/java/example/SparkStreaming.java
@@ -26,6 +26,7 @@ public class SparkStreaming {
             "CAGGTAGAAAGAAGCAGAATCGGA"
     };
 
+    private static final String barcodePrefix = "GCTTGGGTGTTTAACC";
 
     public static void main(String[] args) throws InterruptedException {
         ObjectMapper objectMapper = new ObjectMapper();
@@ -37,11 +38,76 @@ public class SparkStreaming {
 
         JavaDStream<Genome> genomeStream = lines.map((Function<String, Genome>) s -> objectMapper.readValue(s, Genome.class));
 
-//        JavaDStream<Genome> genomesWithBC = lines.
+        JavaDStream<Genome> genomesWithBC = genomeStream.map(genome -> {
+            determineBarcode(genome);
+            return genome;
+        });
 
         genomeStream.print();
 
         jssc.start();              // Start the computation
         jssc.awaitTermination();   // Wait for the computation to terminate}
+    }
+
+    private static void determineBarcode(Genome genome) {
+        int[] scorePerBarcode = new int[BARCODES.length];
+        for(int i = 0; i < BARCODES.length; i++) {
+            String fullString = barcodePrefix + BARCODES[i]; //TODO: Test with barcodePostfix
+            // Using an offset to increase certainty for a given barcode because of multiple tries
+            for(int offset = 0; offset < 10; offset++) {
+                scorePerBarcode[i] += levenshteinDistance(genome.getValue().substring(offset, fullString.length() + offset), fullString);
+            }
+        }
+
+        int indexMinScore = 0;
+        int currentMinScore = scorePerBarcode[0];
+        for(int i = 0; i < scorePerBarcode.length; i++) {
+            if(scorePerBarcode[i] < currentMinScore) {
+                currentMinScore = scorePerBarcode[i];
+                indexMinScore = i;
+            }
+        }
+        genome.setBarcode(BARCODES[indexMinScore]);
+    }
+
+    // https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance#Java
+    private static int levenshteinDistance(String a, String b) {
+        int len0 = a.length() + 1;
+        int len1 = b.length() + 1;
+
+        // the array of distances
+        int[] cost = new int[len0];
+        int[] newcost = new int[len0];
+
+        // initial cost of skipping prefix in String s0
+        for (int i = 0; i < len0; i++) cost[i] = i;
+
+        // dynamically computing the array of distances
+
+        // transformation cost for each letter in s1
+        for (int j = 1; j < len1; j++) {
+            // initial cost of skipping prefix in String s1
+            newcost[0] = j;
+
+            // transformation cost for each letter in s0
+            for(int i = 1; i < len0; i++) {
+                // matching current letters in both strings
+                int match = (a.charAt(i - 1) == b.charAt(j - 1)) ? 0 : 1;
+
+                // computing cost for each transformation
+                int cost_replace = cost[i - 1] + match;
+                int cost_insert  = cost[i] + 1;
+                int cost_delete  = newcost[i - 1] + 1;
+
+                // keep minimum cost
+                newcost[i] = Math.min(Math.min(cost_insert, cost_delete), cost_replace);
+            }
+
+            // swap cost/newcost arrays
+            int[] swap = cost; cost = newcost; newcost = swap;
+        }
+
+        // the distance is the cost for transforming all letters in both strings
+        return cost[len0 - 1];
     }
 }
